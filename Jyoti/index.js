c5377caa8e1f22406520d4cd44f79f35224b73af
Matthew Johnson <mattmjohnson33@gmail.com>
2018-04-16 16:53:03 -0700

    made clicking on the pencil remove a readonly field from the input and add it back when clicking away, if text changes then a put method is fired to the server to change the data on the backend

--- Jyoti/index.js
+++ Jyoti/index.js
@@ -1,28 +1,74 @@
-// var source   = document.getElementById("entry-template").innerHTML;
-// var template = Handlebars.compile(source);
-//
-// var context = {title: "My New Post", body: "This is my first post!"};
-// var html = template(context);
-//
-// var placeHolder = document.getElementById("placeHolder");
-// placeHolder.innerHTML = html;
-
-
-var source   = document.getElementById("entry-template").innerHTML;
-var template = Handlebars.compile(source);
-
-var context = {name: "Jose", location: "New York", birthday: "12/24/1992", occupation: "programmer", relationshipStatus:"single", interestedIn:"meditation"};
-var context2 = {name: "Bob", location: "Japan", birthday: "12/24/1992", occupation: "programmer", relationshipStatus:"single", interestedIn:"meditation"};
-
-var html = template(context2);
-
-var placeHolder = document.getElementById("placeHolder");
-placeHolder.innerHTML = html;
-
-$.ajax({
+var aboutMeData = $.ajax({
   method: "GET",
-  url: "http://localhost:3000/user",
-  dataType: "jsonp"
+  url: "http://localhost:3000/user/1",
+  dataType: "jsonp",
+  success: function (data) {
+        // Send data back to the locale variable
+        aboutMeData = data;
+  }
 }).then(function(data){
   console.log('data',data);
-})
+  myTemplateFunction(data);
+  addEventListeners();
+});
+
+
+var myTemplateFunction = function(templateData){
+  var source   = document.getElementById("entry-template").innerHTML;
+  var template = Handlebars.compile(source);
+
+  var html = template(templateData);
+
+  var placeHolder = document.getElementById("placeHolder");
+  placeHolder.innerHTML = html;
+}
+
+var addEventListeners = function(){
+  var aboutMeList = document.querySelector('ul');
+  aboutMeList.addEventListener('click', function($event){
+    $event.stopPropagation();
+    if($($event.target).hasClass('fieldEditor')){
+      var fieldToEdit = $($event.target).parent('li').find('input')[0];
+      var fieldCurrentText = fieldToEdit.value;
+      $(fieldToEdit).removeAttr('readonly');
+
+      fieldToEdit.addEventListener('focusout', function($event){
+        $(fieldToEdit).prop('readonly', true);
+        if(fieldCurrentText !== fieldToEdit.value){
+          updateUserObject($(fieldToEdit).attr('model'), fieldToEdit.value, aboutMeData);
+        }
+      });
+    }
+  });
+}
+
+
+var updateUserObject = function(model, value, aboutMeData){
+  var dataForPassing = {model:value};
+  $.ajax({
+    method: "PUT",
+    url: "http://localhost:3000/user/1",
+    dataType: "jsonp",
+    data: dataForPassing,
+    contentType: 'application/json',
+    success: function(msg){
+      console.log('dataForPassing', dataForPassing);
+      console.log('msg',msg);
+    }
+  })
+}
+
+
+
+
+
+// url: "http://localhost:3000/user/1?" + model + "=" + value,
+
+
+
+
+
+
+
+
+// kjkj
