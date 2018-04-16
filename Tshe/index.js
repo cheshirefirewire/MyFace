5a91bc8b30cf6fe937cfc01a8be233e79990b951
Tshewang Dorji <tshewangdorji@Tshewangs-MacBook-Pro.local>
2018-04-16 12:59:52 -0700

    I update the file

--- Tshe/index.js
+++ Tshe/index.js
@@ -1,21 +1,59 @@
-var source   = document.getElementById("entry-template").innerHTML;
+/*var myData = $.ajax({
+    method: "GET",
+    url: "http://localhost:3000/user",
+    dataType: "jsonp"
+    success: function (data) {
+
+        myDate = date;
+    }
+}).then(function (data) {
+    myTemplateFunction(Mydata[0]);
+    addEventListener();
+});*/
+
+
+
+// ProfileImg
+(function profileImg(){
+var source = document.getElementById("profileImg").innerHTML;
 var template = Handlebars.compile(source);
 
-var context = { Name: "Tshe", Location: "Blouder Creek", Birthday: "03/02/1990", Occupation: "Thinking", Relationship: "Single", InterestedIn: "None"};
-var html    = template(context);
+var context = {profile_img: "image/profile.png" };
+
+var htmlTemplate = template(context);
+
+var profileImg = document.getElementById("profile-holder");
+profileImg.innerHTML = htmlTemplate;
+
+// // Nav
+var source = document.getElementById("asideNav").innerHTML;
+var template = Handlebars.compile(source);
+
+var context = { 
+    Name: "Tshe", 
+    Location: "Blouder Creek", 
+    Birthday: "03/02/1990", 
+    Occupation: "Thinking", 
+    Relationship: "Single", 
+    InterestedIn: "None" };
+
+var html = template(context);
 
 
-var placeholder = document.getElementById("placeholder");
+var placeholder = document.getElementById("navHolder");
 placeholder.innerHTML = html;
-
-
-
-
-var source   = document.getElementById("banner-img").innerHTML;
-var template = Handlebars.compile(source);
-
-var context1 = {profile_img: "./image/profile.png" };
-var htmlTemplate   = template(context1);
-
-var bannerImg = document.getElementById("banner-img");
-bannerImg.innerHTML = htmlTemplate;
+})();
+/*
+$(document).ready(function){
+    var aboutMylist = document.querySelector('ul');
+    aboutMylist.addEventListener('event', function ($event) {
+        if ($($event.targe).hasClass('fieldEditor')) {
+            var fieldToEdit = $($event.target).parent('li').find('input')[0];
+            $(fieldEditor).removeAttr('readonly');
+            document.addEventListener('click', function ($event) {
+                $(fieldEditor).attr('readonly', 'true');
+            });
+        }
+    });
+}
+*/
