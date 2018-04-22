34496ca0e276f19661ea15b184b8f8767dcdf8a5
sergio4156 <sergio4156@gmail.com>
2018-04-21 21:04:26 -0700

    background and profile images rendering in html from AJAX call added side nav functionality to allow user to edit about me data

--- Sergio/index.js
+++ Sergio/index.js
@@ -1,41 +1,60 @@
-(function aboutNavBarInfo() {
+"use strict";
+
+var aboutMeData = $.ajax({
+  method: "GET",
+  url: "http://localhost:3000/user/1",
+  dataType: "jsonp",
+  success: function (data) {
+        // Send data back to the locale variable
+        aboutMeData = data;
+  }
+}).then(function(data){
+  console.log('data',data);
+  myTemplateFunction(data);
+  profilePic(data);
+  backgroundImage(data)
+  addEventListenersToSideNav();
+});
+
+
+//no IIFE for myTemplateFunction because it's being called within AJAX .then annonymous function
+function myTemplateFunction(templateData) {
 	var source = document.getElementById("side-nav-template").innerHTML;
 	var sideNavTemplate = Handlebars.compile(source);
-
-	var context = {name: "I AM GREAT", location: "San Fran baby", birthday: "March", occupation: "UI Dev", relationshipStatus: "with Light!", interested: "Spiritual Clarity"};
-	var templateHTML = sideNavTemplate(context);
-
+	var templateHTML = sideNavTemplate(templateData);
 	var sideNavPlaceHolder = document.getElementById("side-nav-placeHolder-div");
 	sideNavPlaceHolder.innerHTML = templateHTML;
-})();
+}
 
-(function profilePic() {
+
+//Function to populate html profile pic
+function profilePic(profilePicData) {
 	var source = document.getElementById("profile-pic-template").innerHTML;
 	var profilePicTemplate = Handlebars.compile(source);
 
-	var context = {profilePic: "images/profile.png"};
-	var profilePic = profilePicTemplate(context);
+	//var context = {profilePic: "images/profile.png"};
+	var profilePic = profilePicTemplate(profilePicData);
 
 	var profilePicPlaceHolder = document.getElementById("profile-pic-placeHolder-div");
 	profilePicPlaceHolder.innerHTML = profilePic;
-})();
+};
 
-(function backgroundImage() {
+//Function to populate html background image
+function backgroundImage(backgroundImageData) {
 	var source = document.getElementById("background-pic-template").innerHTML;
 	var backgroundPicTemplate = Handlebars.compile(source);
 
-	var context = {backgroundPic: "images/nature.jpg"};
-	var backgroundPic = backgroundPicTemplate(context);
+	//var context = {backgroundPic: "images/nature.jpg"};
+	var backgroundPic = backgroundPicTemplate(backgroundImageData);
 
 	var backgroundPicPlaceHolder = document.getElementById("background-pic-placeHolder-div");
 	backgroundPicPlaceHolder.innerHTML = backgroundPic;
-})();
+}
 
+//Function to toggle side nav vertically up and down
 function toggleVerticalNavUpDown(elem) {
-
 	var target = document.getElementById(elem);
-	var maxHeight = "340px";
-
+	var maxHeight = "auto";
 	if(target.style.height === maxHeight) {
 		document.getElementById("arrow").classList.toggle("fa-arrow-circle-down");
 		target.style.height = "0px";
@@ -44,3 +63,66 @@ function toggleVerticalNavUpDown(elem) {
 		target.style.height = maxHeight;
 	}
 }
+
+//Function to allow user to enter new values into input fields and send new data to DB
+function addEventListenersToSideNav() {
+	var aboutMeList = document.querySelector('#verticalSide-nav-ul');
+	aboutMeList.addEventListener('click', function(event) {
+		event.stopPropagation();
+		if(event.target.className === 'fieldEditor') {
+			var fieldToEdit = event.target.previousElementSibling;
+			fieldToEdit.removeAttribute('readonly');
+			var fieldCurrentText = fieldToEdit.value;
+			//console.log("focusin successful");
+
+			fieldToEdit.addEventListener('focusout', function(event) {
+				fieldToEdit.readonly = true;
+				//console.log("focusout successful");
+				if(fieldCurrentText != fieldToEdit.value) {
+					updateUserObject($(fieldToEdit).attr('model'), fieldToEdit.value, aboutMeData);
+					//console.log("new value entered!");
+				}
+			});
+		}
+	});
+}
+
+
+/*
+var addEventListeners = function(){
+  var aboutMeList = document.querySelector('ul');
+  aboutMeList.addEventListener('click', function($event){
+    $event.stopPropagation();
+    if($($event.target).hasClass('fieldEditor')){
+    	console.log("testing!!");
+      var fieldToEdit = $($event.target).parent('li').find('input')[0];
+      var fieldCurrentText = fieldToEdit.value;
+      $(fieldToEdit).removeAttr('readonly');
+
+      fieldToEdit.addEventListener('focusout', function($event){
+        $(fieldToEdit).prop('readonly', true);
+        if(fieldCurrentText !== fieldToEdit.value){
+          updateUserObject($(fieldToEdit).attr('model'), fieldToEdit.value, aboutMeData);
+        }
+      });
+    }
+  });
+}
+*/
+
+var updateUserObject = function(model, value, aboutMeData){
+  var dataForPassing = {model:value};
+  $.ajax({
+    method: "PUT",
+    url: "http://localhost:3000/user/1",
+    dataType: "jsonp",
+    data: dataForPassing,
+    contentType: 'application/json',
+    success: function(msg){
+      console.log('dataForPassing', dataForPassing);
+      console.log('msg',msg);
+    }
+  })
+}
+
+
