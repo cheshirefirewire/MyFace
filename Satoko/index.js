478fc6a880998777de1bbb0fa0588bf6b1442f48
Matthew Johnson <mattmjohnson33@gmail.com>
2018-05-05 15:32:23 -0700

    adding Satoko's changes

--- Satoko/index.js
+++ Satoko/index.js
@@ -1,48 +1,98 @@
 $(document).ready(function(){
 
-	(function coverImgTemp() {
+	var myData = $.ajax({
+		method: 'GET',
+		url: 'http://localhost:3000/user',
+		dataType: 'jsonp',
+		success: function (data) {
+			myData = data;
+		}
+	}).then(function(data){
+		init();
+	});
+
+	var init = function() {
+		userInfoTemp(myData[0]);
+		profImgTemp(myData[0].profileImage);
+		coverImgTemp(myData[0].backgroundImage)
+		addEventListeners();
+	};
+
+	var coverImgTemp = function(coverImgData) {
 		var source   = document.getElementById('cover-img-template').innerHTML;
 		var template = Handlebars.compile(source);
 
-		var context = {coverImg: 'images/cover.jpg'};
+		var context = {coverImg: 'images/' + coverImgData};
 		var templateHtml = template(context);
 
 		var placeholder = document.getElementById('coverOuter');
 		placeholder.innerHTML = templateHtml;
-	})();
+	};
 
-	(function profImgTemp() {
+	var profImgTemp = function(profImgData) {
 		var source   = document.getElementById('profile-img-template').innerHTML;
 		var template = Handlebars.compile(source);
 
-		var context = {profileImg: 'images/profile.png'};
+		var context = {profileImg: 'images/' + profImgData};
 		var templateHtml = template(context);
 
 		var placeholder = document.getElementById('profileOuter');
 		placeholder.innerHTML = templateHtml;
-	})();
+	};
 
-	(function aboutTemp(){
+	var userInfoTemp = function(templateData){
 		var source   = document.getElementById('about-template').innerHTML;
 		var template = Handlebars.compile(source);
 
-		var context = {"name":"Jose", "location": "New York", "birthday": "12/24/1992", "occupation": "programmer", "relationshipStatus":"single", "interestedIn":"meditation"};
-		var templateHtml = template(context);
-		console.log('template: ', template)
+		var templateHtml = template(templateData);
 
 		var placeholder = document.getElementById('about');
 		placeholder.innerHTML = templateHtml;
-	})();
+	};
 
 	$("#aboutHeader").click(function(){
 	    $("#aboutList").toggle();
 	});
 
+	var addEventListeners = function(){
+		var aboutMeList = document.getElementById('aboutList');
+		aboutMeList.addEventListener('click', function(event){
+
+			if($(event.target).hasClass('pencil')){
+
+				var fieldToEdit = $(event.target).prev();
+				var fieldInputValue = fieldToEdit.val();
+
+				fieldToEdit.removeAttr('readonly');
+				fieldToEdit.on('focusout', function(event){
+					fieldToEdit.attr('readonly', 'true');
+					console.log('fieldToEdit value', fieldToEdit.val());
+					console.log('fieldInputValue', fieldInputValue);
+
+					if(fieldToEdit.val() !== fieldInputValue){
+						console.log('updateUserObject');
+        				updateUserObject(fieldToEdit.attr('label'), fieldToEdit.val(), myData);
+					}
+				});
+			};
+		});
+	};
+
+	var updateUserObject = function(label, value, myData){
+	  var dataForPassing = {label:value};
+	  $.ajax({
+	    method: "PUT",
+	    url: "http://localhost:3000/user/1",
+	    dataType: "jsonp",
+	    data: dataForPassing,
+	    contentType: 'application/json',
+	    success: function(msg){
+	      console.log('dataForPassing', dataForPassing);
+	      console.log('msg',msg);
+	    }
+	  })
+	}
+
+
 });
 
-// $.ajax({
-// 	method: "POST",
-// 	url: "data.json",
-// 	data: {}
-// })
-// .done(function( data ));
