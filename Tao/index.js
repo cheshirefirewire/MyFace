f7337721eb121ad70b3674b9e7ea4cd15e911c1d
Macadamia <macadamia@Macadamias-MacBook-Pro.local>
2018-05-07 10:17:49 -0700

    changed templates to insert image into div

--- Tao/index.js
+++ Tao/index.js
@@ -30,21 +30,9 @@
 
 (function bannerImage(){
   var source   = document.getElementById("banner-template").innerHTML;
-  console.log('source is ', source);
   var template = Handlebars.compile(source);
-  console.log('template is ', template);
   var context = { myBanner: "url(./images/cover.jpg)" };
-  console.log('context is ', context);
   var html    = template(context);
-  console.log('html is ', html);
-  // var placeholder = document.createElement('div');
-  // console.log('placeholder is ', placeholder);
-  // placeholder.innerHTML = html;
-  // console.log('placeholder is now ', placeholder);
-  // var bannerdiv = placeholder.lastChild;
-  // console.log('bannerdiv is ', bannerdiv);
   var header = document.getElementById("header");
-  console.log('header is ', header);
   header.insertAdjacentHTML('afterbegin', html);
-  console.log('header is now ', header);
 })();
