57e27f7c83c32891b1d7515f5904ebca25ddad03
LAPTOP-V42UMJO1\karina <starcoder108@outlook.com>
2018-05-07 22:38:22 -0700

    Layout and styling

---
+++ Karina/index.js
@@ -0,0 +1,11 @@
+/*
+var source   = document.getElementById("entry-template").innerHTML;
+var template = Handlebars.compile(source);
+
+var context = {title: "Karina's New Post", body: "This is my first post!"};
+var templateHtml = template(context);
+
+var placeholder = document.getElementById("placeholder");
+placeholder.innerHTML = templateHtml;
+
+*/
