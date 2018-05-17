aff0a9883bbe3ef53aefdeac0957173d2eabf373
cclen <cece.len12@gmail.com>
2018-05-16 19:01:41 -0700

    adding ajax PATCH method in order to update user data

--- Cece/scripts/index.js
+++ Cece/scripts/index.js
@@ -27,7 +27,7 @@
 // GET JSON DATA, USE IT IN THE ABOUT TEMPLATE
 var aboutMeData = $.ajax({
 	method: "GET",
-	url: "http://localhost:3000/users",
+	url: "http://localhost:3000/users/2",
 	dataType: "json",
 	success: function(data){
 		// Send data back to the locale variable
@@ -42,7 +42,7 @@ var aboutMeTemplateFunction = function(templateData){
 	var source   = document.getElementById("about-template").innerHTML;
 	var template = Handlebars.compile(source);
 
-	var templateHtml = template(templateData[1]);
+	var templateHtml = template(templateData);
 
 	var placeHolder = document.getElementById("about");
 	placeHolder.innerHTML = templateHtml;
@@ -57,15 +57,40 @@ var listenToAboutMeFieldChanges = function(){
 		var target = $event.target;
 		if($(target).hasClass("glyphicon-pencil")){
 			var fieldToEdit = $(target).closest("li").find("input")[0];
-			var fieldCurrentText = fieldToEdit.value;
+			var fieldCurrentData = fieldToEdit.value;
 			$(fieldToEdit).attr("readonly", false);
 
 			fieldToEdit.addEventListener("focusout", function($event){
 				$(fieldToEdit).attr("readonly", true);
+				if(fieldCurrentData !== fieldToEdit.value) {
+					var fieldName = $(fieldToEdit).attr("name");
+					updateUserObject(fieldName, fieldToEdit.value);
+				}
 			});
 		}
 	});
 };
+
+var updateUserObject = function(prop, value){
+	var dataForPassing = {};
+	dataForPassing[prop] = value;
+
+	$.ajax({
+		method: "PATCH",
+		url: "http://localhost:3000/users/2",
+		dataType: "json",
+		data: JSON.stringify(dataForPassing),
+		contentType: "application/json",
+		success: function(data){
+			console.log("dataForPassing success: ", dataForPassing);
+			console.log("data", data);
+		},
+		error: function(error){
+			console.log("dataForPassing error: ", dataForPassing);
+			console.log("error", error);
+		}
+	});
+}
 
 // var blogs = $ajax({
 // 	method: "GET",
